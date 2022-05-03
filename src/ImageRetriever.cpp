0416d16fbf1b2b904852376bc2288e67ff2cd969
sallyyjan <sallyujeongan@gmail.com>
2022-05-02 20:08:17 -0700

    filePath init

--- src/ImageRetriever.cpp
+++ src/ImageRetriever.cpp
@@ -271,7 +271,7 @@ void ImageRetriever::getImage(string &imagePath, long * timestamp, bool getTimes
             throw e;
         }
 
-        imageNumber++
+        imageNumber++;
 
         // Save image to the temporary saving location
         ostringstream filePath;
@@ -312,7 +312,7 @@ void ImageRetriever::getImage(string &imagePath, long * timestamp, bool getTimes
             return;
         }
 
-        ostringstream filePath;
+        ostringstream filePath();
         filePath << imageFilePath << "/" << acquisitionStartTime << "-" << imageNumber++ << ".jpg";
         imwrite(filePath.str(), image);
 

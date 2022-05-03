ad21aaeddf1d770c2d133a417a463a8aca55d040
sallyyjan <sallyujeongan@gmail.com>
2022-05-02 21:12:41 -0700

    error code catch

--- src/ImageRetriever.cpp
+++ src/ImageRetriever.cpp
@@ -291,9 +291,9 @@ void ImageRetriever::getImage(string &imagePath, long * timestamp, bool getTimes
             experimental::filesystem::rename(filePath, newFilePath, ec);
             
             // if rename does not throw error, then
-            if (ec != null)
+            if (ec.value() != 0)
             {
-                cout << ec.what() << '\n';
+                cout << error_code::message(ec.value) << '\n';
                 imagePath = "";
                 return;
             }

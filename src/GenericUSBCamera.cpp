80c0e38d385b51a2800217ad3f36aa2835ab0fcc
sallyyjan <sallyujeongan@gmail.com>
2022-05-08 00:45:56 -0500

    v

--- src/GenericUSBCamera.cpp
+++ src/GenericUSBCamera.cpp
@@ -26,7 +26,7 @@ bool GenericUSBCamera::openCamera()
             return false;
         }
     }
-    cout << "!!!!!!!!!!!!!!!!!!! fr" << VideoCapture.get(CAP_PROP_FPS) << endl;
+    cout << "!!!!!!!!!!!!!!!!!!! fr" << videoCapture.get(CAP_PROP_FPS) << endl;
     return true;
 }
 

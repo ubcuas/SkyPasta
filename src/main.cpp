a5d3e20a7aec83c6c2be14a51b1a7ae372594042
sallyyjan <sallyujeongan@gmail.com>
2022-05-08 01:55:10 -0500

    1.2 spf

--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,7 @@ void acquireImages(ImageRetriever *imageRetriever)
 {
     while (!stopFlag)
     {
-        sleep(0.5); // for latency
+        sleep(1.2); // for latency
         //std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
         imageRetriever->acquireImage(tagImages);
